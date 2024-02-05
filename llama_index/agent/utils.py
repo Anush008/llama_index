740c15712de67becfb6a87a3a68e237554f39695
Marcus Schiesser <mail@marcusschiesser.de>
2024-02-05 11:26:58 +0700

    refactor: add create_agent_from_llm method to create the right agent runner depending on the LLM

--- llama_index/agent/utils.py
+++ llama_index/agent/utils.py
@@ -1,9 +1,13 @@
 """Agent utils."""
 
 
+from typing import Any, Optional
+
+from llama_index.agent.runner.base import AgentRunner
 from llama_index.agent.types import TaskStep
 from llama_index.core.llms.types import MessageRole
 from llama_index.llms.base import ChatMessage
+from llama_index.llms.llm import LLM
 from llama_index.memory import BaseMemory
 
 
@@ -15,3 +19,23 @@ def add_user_step_to_memory(
     memory.put(user_message)
     if verbose:
         print(f"Added user message to memory: {step.input}")
+
+
+def create_agent_from_llm(
+    llm: Optional[LLM] = None,
+    **kwargs: Any,
+) -> AgentRunner:
+    from llama_index.agent import OpenAIAgent, ReActAgent
+    from llama_index.llms.openai import OpenAI
+    from llama_index.llms.openai_utils import is_function_calling_model
+
+    if isinstance(llm, OpenAI) and is_function_calling_model(llm.model):
+        return OpenAIAgent.from_tools(
+            llm=llm,
+            **kwargs,
+        )
+    else:
+        return ReActAgent.from_tools(
+            llm=llm,
+            **kwargs,
+        )
